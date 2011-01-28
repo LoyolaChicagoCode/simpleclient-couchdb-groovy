a7458c2cd850f376d66d00f051e5a7e29acf5c59
Konstantin Laufer <laufer@cs.luc.edu>
2011-01-28 16:30:06 -0600

    Cleaned up paths to resources.

--- CouchDBSimpleClient.groovy
+++ CouchDBSimpleClient.groovy
@@ -2,10 +2,10 @@
 import static groovyx.net.http.ContentType.JSON
 import groovyx.net.http.RESTClient
 
-def client = new RESTClient("https://laufer.cloudant.com/blah")
+def client = new RESTClient("https://laufer.cloudant.com/blah/")
 client.auth.basic "themphybuldeatheyeseetud", "cJEftnTFJAjGEobnbljsoh65"
 
-def resp = client.get(path: "blah/_all_docs", contentType: JSON)
+def resp = client.get(path: "_all_docs", contentType: JSON)
 
 resp.data.rows.each {
   println it
